e17c1e2f352be657727e45b08db75b5dfa5d60bb
Yanisse <yanisse321@gmail.com>
2022-12-03 12:21:06 +0100

    Update admin interface

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -6,14 +6,26 @@ use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArticleType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', TextType::class, [
+                'label' => "Titre de l'article",
+                'attr'=>[
+                    'class'=>'form-control'
+                ]
+            ])            
+            ->add('content', TextareaType::class, [
+                'label' => "Contenu de l'article",
+                'attr'=>[
+                    'class'=>'form-control'
+                ]
+            ])
         ;
     }
 
